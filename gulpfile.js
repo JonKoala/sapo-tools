5133a0aa6b6af35c0a6e9174de13d2385cd34461
JonKoala <victor.santana.lemos@gmail.com>
2017-08-30 16:18:26 -0300

    adicionando etapa git reset antes de realizar o pull

--- gulpfile.js
+++ gulpfile.js
@@ -8,28 +8,35 @@ var fs = require('fs');
 var configFile = fs.readFileSync('appconfig.json');
 var appconfig = JSON.parse(configFile);
 
-gulp.task('update-api', (done) => {
+gulp.task('reset-api', done => {
+  git.reset('origin/master', {args:'--hard', cwd: appconfig.path.api}, done);
+});
+gulp.task('reset-client', done => {
+  git.reset('origin/master', {args:'--hard', cwd: appconfig.path.client}, done);
+});
+
+gulp.task('update-api', ['reset-api'],  done => {
   git.pull('origin', 'master', {cwd: appconfig.path.api}, done);
 });
-gulp.task('update-client', (done) => {
+gulp.task('update-client', ['reset-client'],  done => {
   git.pull('origin', 'master', {cwd: appconfig.path.client}, done);
 });
 
-gulp.task('install-dependencies-api', ['update-api'], (done) => {
+gulp.task('install-dependencies-api', ['update-api'], done => {
   gulp.src([appconfig.path.api+'package.json']).pipe(install(done));
 });
-gulp.task('install-dependencies-client', ['update-client'], (done) => {
+gulp.task('install-dependencies-client', ['update-client'], done => {
   gulp.src([appconfig.path.client+'package.json', appconfig.path.client+'bower.json']).pipe(install(done));
 });
 
-gulp.task('start-api', ['install-dependencies-api'], (done) => {
+gulp.task('start-api', ['install-dependencies-api'], done => {
   child.spawn('npm.cmd', ['start'], {cwd: appconfig.path.api}, (err, stdout, stderr) => {
     console.log(stdout);
     console.log(stderr);
     done(err);
   });
 });
-gulp.task('start-client', ['install-dependencies-client'], (done) => {
+gulp.task('start-client', ['install-dependencies-client'], done => {
   child.spawn('npm.cmd', ['start'], {cwd: appconfig.path.client}, (err, stdout, stderr) => {
     console.log(stdout);
     console.log(stderr);
@@ -37,13 +44,15 @@ gulp.task('start-client', ['install-dependencies-client'], (done) => {
   });
 });
 
-gulp.task('open-browser', ['install-dependencies-api', 'install-dependencies-client'], (done) => {
+gulp.task('open-browser', ['install-dependencies-api', 'install-dependencies-client'], done => {
   setTimeout(() => {
-    gulp.src(__filename).pipe(open({uri: appconfig.url}, done));
+    gulp.src(__filename).pipe(open({uri: appconfig.url, app: 'chrome'}, done));
   }, 1000);
 });
 
 gulp.task('default', [
+  'reset-api',
+  'reset-client',
   'update-api',
   'update-client',
   'install-dependencies-api',

107dbf901b052a5aa9fe59b0511ea8be42b4457c
JonKoala <victor.santana.lemos@gmail.com>
2017-09-19 15:18:29 -0300

    adicionando logging

--- scraper_routine.py
+++ scraper_routine.py
@@ -1,9 +1,9 @@
 import re
 import csv
 import json
+import logging
+from inspect import getframeinfo, stack
 from test_case_el import TestCaseEl
-
-import pprint
 
 TEST_MONTH_RADIUS = 6
 TEST_PN_CASES = 6
@@ -40,14 +40,46 @@ def output_result(result):
     with open('{}.json'.format(FILE_RESULTS_NAME), 'w') as file_ref:
         file_ref.write(text)
 
+def log(message, error=False):
 
+    print(message)
+
+    #getting caller data
+    caller = getframeinfo(stack()[1][0])
+    data = '[{}:{}] {}'.format(caller.filename, caller.lineno, message)
+    
+    log_ref = logging.exception if error else logging.info
+    log_ref(data)
+
+
+##
+#routine
+
+logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-8s %(message)s',
+    datefmt='%Y-%m-%d:%H:%M:%S',
+    level=logging.DEBUG,
+    filename='assets/log.txt')
+
+log('\n - starting new scraper routine -')
+
+log('extracting data from source (\'{}.csv\')'.format(FILE_SOURCE_NAME))
 input_data = get_input_csv()
+log('done extracting data ({} test cases)'.format(len(input_data)))
 
+log('executing test routines')
 results = []
-for data in input_data:
-    result = execute_tests(data)
-    result['profile'] = data
-    results += [result]
+for index, data in enumerate(input_data):
+    try:
+        log('testing {}th case ({} - {})'.format(index+1, data['municipio'], data['poder']))
+        result = execute_tests(data)
+        result['profile'] = data
+        results += [result]
+    except:
+        log('failed testing {}th case ({} - {})'.format(index+1, data['municipio'], data['poder']), True)
+log('done executing test routines')
 
+log('printing results to file (\'{}.json\')'.format(FILE_RESULTS_NAME))
 output_result(results)
-print('end')
+log('done printing results to file')
+
+log('\n - ending scraper routine - \n')

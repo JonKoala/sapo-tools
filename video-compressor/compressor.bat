6ceffd7a63bc314f109a6269cef73fed0ebe9b98
JonKoala <victor.santana.lemos@gmail.com>
2017-10-25 18:15:18 -0200

    adicionando arquivo bat para facilitar o uso

---
+++ video-compressor/compressor.bat
@@ -0,0 +1,13 @@
+@echo off
+
+rem put node, npm, and ffmpeg in the PATH
+set "PATH=%APPDATA%\npm;%~dp0;%PATH%"
+set "PATH=%APPDATA%\ffmpeg\bin;%PATH%"
+
+rem execute node command
+call npm start
+
+echo.
+echo Press any key to exit
+
+PAUSE >nul
